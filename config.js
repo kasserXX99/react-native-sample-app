5784c8ae71a020bb2ef14c716412f6d40e512aed
ameralshabebi <69112877+ameralshabebi@users.noreply.github.com>
2022-12-06 14:36:02 +0300

    initial commet

---
+++ config.js
@@ -0,0 +1,8 @@
+export const config = {
+  currency: 'SAR',
+  terminalId: 'royat',
+  password: 'royat@123',
+  key: 'b1f6d2c90efecc2c2951ee26619aebd46a370227f1435855a1f2953b0c94e697',
+  requestUrl:
+    'https://payments-dev.urway-tech.com/URWAYPGService/transaction/jsonProcess/JSONrequest',
+};
